0f2e941e980f5d594b85012a42eb52d6196a026d
ik6cgsg <kozlovilya@bk.ru>
2021-10-27 10:38:57 +0300

    add test image generation for N random objects

--- intelligent_checker_lib/intelligent_checker.py
+++ intelligent_checker_lib/intelligent_checker.py
@@ -1,51 +1,49 @@
-from intelligent_placer_lib import intelligent_placer
+import argparse
+import glob
+import random
+#from intelligent_placer_lib import intelligent_placer
 import numpy as np
 import cv2
 from typing import List
 import matplotlib.pyplot as plt
 import skimage.transform
-
-
-from common import common
+import common.image
+import common.common
 
 TAG = '[intelligent_checker]'
 
 
-def cut_out_object(image: np.ndarray, mask: np.ndarray):
-    return cv2.bitwise_and(image.astype("uint8") + 1, image.astype("uint8") + 1,
-                           mask=mask.astype("uint8"))
-
-
-def make_test_image(object_images: List[np.ndarray],
-                    object_grayscale_images: List[np.ndarray],
-                    background_image: np.ndarray,
-                    objects: List[np.ndarray]) -> np.ndarray:
-    # extract objects
-    objects = []
-    for i, image in enumerate(object_images):
-        # cut-out object
-        mask = common.extract_object_masks(object_grayscale_images[i])[0]
-        cut_out = cut_out_object(image, mask)
-
-        a4_mask = common.extract_background_mask(object_grayscale_images[i])
-        _, k = common.get_perspective_matrix_and_scale(a4_mask)
+def generate_test_image(
+    object_images: List[np.ndarray],
+    object_grayscale_images: List[np.ndarray],
+    background_image: np.ndarray,
+    objects_num: int
+) -> np.ndarray:
+    objects: List[np.ndarray] = []
+    indices = random.sample(range(len(object_images)), objects_num)
+    # cut out objects
+    for i in indices:
+        mask = common.common.extract_object_masks(object_grayscale_images[i])[-1]
+        cut_out = common.common.cut_out_object(object_images[i], mask)
+        a4_mask = common.common.extract_background_mask(object_grayscale_images[i])
+        _, k, _, _ = common.common.get_perspective_matrix_and_scale(a4_mask)
         scale = 2 * k
         m = np.float32([
             [scale, 0, cut_out.shape[1] * (1 - scale) / 2],
             [0, scale, cut_out.shape[0] * (1 - scale) / 2]
         ])
         cut_out = cv2.warpAffine(cut_out, m, (cut_out.shape[1], cut_out.shape[0]))
-
+        common.image.show(cut_out, "cut out")
         objects.append(cut_out)
 
-        common.save_image(cut_out, "object" + str(i + 1) + "_cut.jpg")
-
     result = background_image.copy()
-
+    # linear shift
     for i, object_image in enumerate(objects):
         shift = 450
         x = 0 #-shift // 2 + shift * i / (len(objects) - 1)
-        y = -shift // 2 + shift * i / (len(objects) - 1)
+        y = 0
+        if len(objects) > 1:
+            y = -shift // 2 + shift * i / (len(objects) - 1)
         # angle = (i + 1) * 2 * 3.14 / 3
         #
         # m = np.float32([[1, 0, -object_image.shape[1] // 2], [0, 1, -object_image.shape[0] // 2]])
@@ -57,36 +55,36 @@ def make_test_image(object_images: List[np.ndarray],
 
         m = np.float32([[1, 0, x], [0, 1, y]])
         object_image = cv2.warpAffine(object_image, m, (object_image.shape[1], object_image.shape[0]))
-
         result[object_image > 0] = object_image[object_image > 0]
 
     return result
 
 
-def test_intelligent_placer():
-    print(f'{TAG} test_intelligent_placer()')
-    common.do_common_stuff()
-    res = intelligent_placer.check_image('30', [(30, 30)])
-    print(f'{TAG} res = {res}')
-    assert res
+def test_n_random_objects_larger_rect(objects, objects_gray, background, max):
+    for i in range(1, max + 1):
+        img = generate_test_image(objects, objects_gray, background, i)
+        common.image.show(img, "test_image_" + str(i))
+    # todo
 
 
 if __name__ == '__main__':
-    print(f'{TAG} start checking...')
-    # test_intelligent_placer()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--images_folder", default="objects/",
+                        type=str, help="local path to 10 images with single objects placed on a4")
+    parser.add_argument("-b", "--background", default="back.jpg",
+                        type=str, help="local path to image with background")
+    parser.add_argument("-r", "--restrictions", default="restrictions.json",
+                        type=str, help="local path to restrictions json")
+    args = parser.parse_args()
 
-    background = common.open_image_rgb("background.jpg")
+    if args.images_folder == "" or args.background == "" or args.restrictions == "":
+        print("Some of params are empty(")
+        exit(0)
 
-    object_names = ["object1.jpg", "object2.jpg", "object3.jpg"]
-    # object_names = ["object1.jpg"]
-    objects = [common.open_image_rgb(name) for name in object_names]
-    objects_gray = [common.open_image(name) for name in object_names]
+    background = common.image.open_image_rgb(args.background)
+    common.image.show(background, "back")
+    object_paths = glob.glob(args.images_folder+"*")
+    objects = [common.image.open_image_rgb(name) for name in object_paths]
+    objects_gray = [common.image.open_image(name) for name in object_paths]
 
-    cut_objects_names = ["object1_cut.jpg", "object2_cut.jpg", "object3_cut.jpg"]
-    cut_objects = [common.open_image_rgb(name) for name in cut_objects_names]
-
-    test_image = make_test_image(objects, objects_gray, background, cut_objects)
-
-    plt.figure()
-    plt.imshow(test_image)
-    plt.show()
+    test_n_random_objects_larger_rect(objects, objects_gray, background, 4)
